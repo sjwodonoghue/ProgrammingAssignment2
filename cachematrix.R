9632b3050f6e1ccd6be9af0656dc60c1d5ba36ac
goya11 <sjwodonoghue@gmail.com>
2014-10-26 13:46:18 +0000

    Implementation of matrix object creation with caching of the matrix inverse.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The below functions implement creating a special matrix object that
+## allows the inverse of the matrix to be computed and stored in the
+## parent/global envirnment.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a matrix object with associated functions that
+## allow you to cache the inverse of the matrix in the parent/global 
+## environment.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+        
+    list(set = set, get = get,
+        setsolve = setsolve,
+        getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## if the inverse of the given matrix is cached cacheSolve will print that value
+## else it will compute the value of the inverse and set the caching variable to
+## be equal to this.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getsolve()
+
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setsolve(m)
+    m
 }
